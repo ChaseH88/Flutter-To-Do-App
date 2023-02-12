e7c09cbba55c732dc0b9a3792762156292d1c23d
Chase Harrison <chaseh88@gmail.com>
2023-02-12 09:26:43 -0600

    add delete functionality

--- lib/main.dart
+++ lib/main.dart
@@ -97,8 +97,10 @@ class _ToDoWidgetState extends State<ToDoWidget> {
 class ToDoListWidget extends StatelessWidget {
   final List<Map<String, dynamic>> toDoArr;
   final Function markToDoItemComplete;
+  final Function removeToDoItem;
 
-  ToDoListWidget({this.toDoArr, this.markToDoItemComplete});
+  ToDoListWidget(
+      {this.toDoArr, this.markToDoItemComplete, this.removeToDoItem});
 
   @override
   Widget build(BuildContext context) {
@@ -106,20 +108,47 @@ class ToDoListWidget extends StatelessWidget {
       child: ListView.builder(
         itemCount: toDoArr.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: toDoArr[index]['isComplete']
-                ? Text(
-                    toDoArr[index]['task'],
-                    style: TextStyle(
-                      decoration: TextDecoration.lineThrough,
+          final item = toDoArr[index];
+
+          return Dismissible(
+            key: Key(item['key']),
+            onDismissed: (direction) {
+              removeToDoItem(item['key']);
+            },
+            child: ListTile(
+              title: item['isComplete']
+                  ? Text(
+                      item['task'],
+                      style: TextStyle(
+                        decoration: TextDecoration.lineThrough,
+                      ),
+                    )
+                  : Text(item['task']),
+              trailing: Checkbox(
+                value: item['isComplete'],
+                onChanged: (value) {
+                  markToDoItemComplete(item['key']);
+                },
+              ),
+            ),
+            background: Container(
+              color: Colors.red,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.delete,
+                      color: Colors.white,
                     ),
-                  )
-                : Text(toDoArr[index]['task']),
-            trailing: Checkbox(
-              value: toDoArr[index]['isComplete'],
-              onChanged: (value) {
-                markToDoItemComplete(toDoArr[index]['key']);
-              },
+                    Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
             ),
           );
         },
