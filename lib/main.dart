945a6faa178291688bc7344beb97026a1b1c06e1
Chase Harrison <chaseh88@gmail.com>
2023-02-12 09:06:05 -0600

    adding initial todo list logic and testing

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,75 @@ void main() {
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext ctx) {
-    return MaterialApp(home: Text('Hello World!'));
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ToDoWidget(),
+    );
+  }
+}
+
+class ToDoWidget extends StatefulWidget {
+  @override
+  _ToDoWidgetState createState() => _ToDoWidgetState();
+}
+
+class _ToDoWidgetState extends State<ToDoWidget> {
+  List<Map<String, dynamic>> toDoArr = [];
+  String taskInput = '';
+
+  void addToDoItem(String task) {
+    String key = DateTime.now().millisecondsSinceEpoch.toString();
+    setState(() {
+      toDoArr.add({
+        'key': key,
+        'task': task,
+        'isComplete': false,
+      });
+    });
+  }
+
+  void handleSubmit() {
+    addToDoItem(taskInput);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To Do Widget'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Add a To Do Item!',
+            ),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  taskInput = value;
+                });
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: toDoArr.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(toDoArr[index]['task']),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: handleSubmit,
+        tooltip: 'Add To Do Item',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
