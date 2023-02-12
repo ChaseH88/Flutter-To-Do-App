999be98819e2d2c0730b9028423eee3eb9fd3b80
Chase Harrison <chaseh88@gmail.com>
2023-02-12 08:00:57 -0600

    initial app render test

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return MaterialApp(home: Text('Hello World!'));
+  }
+}
